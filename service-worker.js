2967ca296dd25d4fcfa4819335d9b1ea0df51f35
dbzsky <dbzksy@protonmail.com>
2019-08-16 21:16:35 +0200

    workbox

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 console.log('Hello from service-worker.js');
 
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-// if (workbox) {
-//   console.log(`Yay! Workbox is loaded 🎉`);
-// } else {
-//   console.log(`Boo! Workbox didn't load 😬`);
-// }
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
