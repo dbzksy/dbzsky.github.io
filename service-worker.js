9339824023b4f42b7142bfc6b34ab65ea5cf9baa
dbzsky <dbzksy@protonmail.com>
2019-08-16 21:25:22 +0200

    service worker

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,18 @@ console.log('Hello from service-worker.js');
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 0.5 * 24 * 60 * 60, // half a day
+        }),
+      ],
+    })
+  );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
